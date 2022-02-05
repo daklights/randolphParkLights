c86dd7efea5d33e76d1ad2e740b6cca93cb4b590
DAK Lights <github@daklights.com>
2022-02-05 14:19:30 -0500

    Update rpl.php

--- rpl.php
+++ rpl.php
@@ -1,23 +1,16 @@
-Sample Landing Page<br /><br />
-
 <?php
 
+$f = fopen("/sys/class/thermal/thermal_zone0/temp","r");
+$temp = fgets($f);
+fclose($f);
+
 $output = shell_exec('cat /proc/cpuinfo');
-
-echo "<pre>";
-print_r($output);
-echo "</pre>";
-
-echo "<br /><br />";
-
-echo "<pre>";
-print_r($_SERVER);
-echo "</pre>";
-
-echo "<br /><br />";
-
-echo "<pre>";
-print_r($settings);
-echo "</pre>";
+$serial = substr($output, (strpos($output, 'Serial'))+9, 17);
+echo "Serial: " . $serial . "<br />";
+echo "IP Address: " . $_SERVER['SERVER_ADDR'] . "<br />";
+echo "FPP Mode: " . $settings['fppMode'] . "<br />";
+echo "Variant: " . $settings['Variant'] . "<br />";
+echo "Temp (C): " . ($temp/1000) . "<br />";
+echo "Time: " . $_SERVER['REQUEST_TIME'] . "<br />";
 
 ?>
