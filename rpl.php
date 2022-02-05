35d8e8c4bdddb42ef4a91c981652f85459d84d07
DAK Lights <github@daklights.com>
2022-02-05 14:03:12 -0500

    Update rpl.php

--- rpl.php
+++ rpl.php
@@ -2,6 +2,14 @@ Sample Landing Page<br /><br />
 
 <?php
 
+$output = shell_exec('cat /proc/cpuinfo');
+
+echo "<pre>";
+print_r($output);
+echo "</pre>";
+
+echo "<br /><br />";
+
 echo "<pre>";
 print_r($_SERVER);
 echo "</pre>";
