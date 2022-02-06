d49748ca6f059bf30e53e5dfd3eb1c48f8be9e7d
DAK Lights <github@daklights.com>
2022-02-06 15:25:56 -0500

    update

--- rpl.php
+++ rpl.php
@@ -7,17 +7,17 @@
 	print_r($r);
 	echo "</pre>";
 	
-	//echo "<br /><br />";
-	
-	//echo "<pre>";
-	//print_r($settings);
-	//echo "</pre>";
-	
-	//echo "<br /><br />";
-	
-	//echo "<pre>";
-	//print_r($GLOBALS);
-	//echo "</pre>";
+	exec('/sbin/ifconfig', $resultArray);
+	echo "ifconfig result=====<br /><pre>";
+	print_r($resultArray);
+	echo "</pre><br /><br />";
+	$result = implode(",", $resultArray);
+	echo $result . "<br /><br />";
+	$ip = preg_match('/eth0.*?inet\saddr:([\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3})\s/', $result, $matches);
+	echo "matches result=====<br /><pre>";
+	print_r($matches);
+	echo "</pre><br /><br />";
+	$ipAddr = isset($matches[1]) ? $matches[1] : '0.0.0.0';
 	
 	$url = $GLOBALS['pluginBaseUrl'] . "index.php";
 	$data = getDeviceData();
