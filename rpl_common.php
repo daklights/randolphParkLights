0df06621a890240180d7a48d5202c85c9975e2ac
DAK Lights <github@daklights.com>
2022-02-06 15:18:45 -0500

    Update rpl_common.php

--- rpl_common.php
+++ rpl_common.php
@@ -15,10 +15,15 @@
 		$output = shell_exec('cat /proc/cpuinfo');
 		$serial = substr($output, (strpos($output, 'Serial'))+9, 17);
 		
+		exec('/sbin/ifconfig', $resultArray);
+		$result = implode(",", $resultArray);
+		$ip = preg_match('/eth0.*?inet\saddr:([\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3})\s/', $result, $matches);
+		$ipAddr = isset($matches[1]) ? $matches[1] : '0.0.0.0';
+		
 		$response = array(
 			'tempC' => trim($temp),
 			'serial' => trim($serial),
-			'ipAddr' => '192.168.0.0',
+			'ipAddr' => trim($ipAddr),
 			'variant' => $settings['Variant'],
 			'fppMode' => $settings['fppMode'],
 			'time' => time()
