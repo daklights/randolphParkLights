b2c1ea10797960ac536c196beb93b2ddb40d3d05
DAK Lights <github@daklights.com>
2022-02-06 16:05:26 -0500

    Update rpl.php

--- rpl.php
+++ rpl.php
@@ -6,17 +6,5 @@
 	echo "<pre>";
 	print_r($r);
 	echo "</pre>";
-	
-	$url = $GLOBALS['pluginBaseUrl'] . "index.php";
-	$data = getDeviceData();
-	$options = array(
-		'http' => array(
-			'method' => 'POST',
-			'content' => json_encode($data)
-		)
-	);	
-	$context = stream_context_create($options);	
-	$result = file_get_contents($url, false, $context);
-	echo $result;
 
 ?>
