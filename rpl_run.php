4c8126ddef7459f19d656d718eff1d2009227571
DAK Lights <github@daklights.com>
2022-09-04 17:55:09 -0400

    Update rpl_run.php

--- rpl_run.php
+++ rpl_run.php
@@ -27,7 +27,7 @@
 			);
 			$context = stream_context_create($options);
 			$result = file_get_contents($url, false, $context);
-			if (strpos($result,'SUCCESS') === true) {
+			if (stripos($result,'SUCCESS') !== false) {
 				// remote success
 				WriteSettingToFile("reportedSequenceName",$playingData['sequenceName'],$pluginName);
 				$sleepDuration = 5;
