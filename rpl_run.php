d8d516828475dcf0c840583c7596ae0ba035a43f
DAK Lights <github@daklights.com>
2022-09-04 17:45:09 -0400

    Update rpl_run.php

--- rpl_run.php
+++ rpl_run.php
@@ -7,25 +7,43 @@
 	logEntry("Base Url: " . $url);
 	
 	while ($doLoop) {
+		$pluginSettings = parse_ini_file($pluginConfigFile);
 		$url = $GLOBALS['pluginBaseUrl'];
-		$data = getDeviceData();
-		$options = array(
-			'http' => array(
-				'method'  => 'POST',
-				'content' => $data,
-				'header'=>  "Content-Type: application/json; charset=UTF-8\r\n" .
-							"Accept: application/json\r\n"
-				)
-		);
-		$context = stream_context_create($options);
-		$result = file_get_contents($url, false, $context);
+		$deviceData = getDeviceData();
+		$playingData = getCurrentPlayingData();
+		$combined = json_encode(array_merge(json_decode($deviceData,true),json_decode($playingData,true)));
+		$reportedSequenceName = $pluginSettings['reportedSequenceName'];
 		
-		if (strpos($result,'ABORT') === false) {
-			sleep(60);
+		if ($reportedSequenceName != $playingData['sequenceName']) {
+			// song has changed
+			logEntry("New Song Detected: " . $playingData['sequenceName']);
+			$options = array(
+				'http' => array(
+					'method'  => 'POST',
+					'content' => $combined,
+					'header'=>  "Content-Type: application/json; charset=UTF-8\r\n" .
+								"Accept: application/json\r\n"
+					)
+			);
+			$context = stream_context_create($options);
+			$result = file_get_contents($url, false, $context);
+			if (strpos($result,'SUCCESS') === false) {
+				// remote success
+				WriteSettingToFile("reportedSequenceName",$playingData['sequenceName'],$pluginName);
+				$sleepDuration = 5;
+				logEntry("Remote Save Success");
+			} else {
+				// remote error
+				$sleepDuration = 2;
+				logEntry("Remote Save Error");
+			}
 		} else {
-			logEntry("Randolph Park Lights Aborting...");
-			$doLoop = false;
-		}
+			// song has not changed
+			$sleepDuration = 5;
+		}	
+		
+		sleep($sleepDuration);
+
 	}
 	
 ?>
