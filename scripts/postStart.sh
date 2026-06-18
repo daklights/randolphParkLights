742315e982f14aaa078e163fe6d05171541b0072
DAK Lights <github@daklights.com>
2026-06-18 10:31:52 -0400

    Handling dynamic boot times of FPP

--- scripts/postStart.sh
+++ scripts/postStart.sh
@@ -4,9 +4,21 @@
 PLUGINDIR=/home/fpp/media/plugins/randolphParkLights
 LOGFILE=/home/fpp/media/logs/randolphParkLights.log
 
-# Wait 15 seconds for FPP to fully boot and start
-echo "$(date '+%Y-%m-%d %H:%M:%S'): Waiting 15 seconds to ensure FPP is booted and started..." | tee -a "$LOGFILE"
-sleep 15
+# Wait for FPP service to fully start
+echo "$(date '+%Y-%m-%d %H:%M:%S'): Checking to ensure FPP service is booted and started..." | tee -a "$LOGFILE"
+until systemctl is-active --quiet fppd; do
+    echo "$(date '+%Y-%m-%d %H:%M:%S'): Waiting for FPP service to start..." | tee -a "$LOGFILE"
+    sleep 2
+done
+echo "$(date '+%Y-%m-%d %H:%M:%S'): FPP service is running." | tee -a "$LOGFILE"
+
+# Wait for FPP web and API to fully start
+echo "$(date '+%Y-%m-%d %H:%M:%S'): Checking to ensure FPP web and API are running..." | tee -a "$LOGFILE"
+until curl -sf http://127.0.0.1/api/fppd/status >/dev/null; do
+    echo "$(date '+%Y-%m-%d %H:%M:%S'): Waiting for FPP web and API to start..." | tee -a "$LOGFILE"
+    sleep 2
+done
+echo "$(date '+%Y-%m-%d %H:%M:%S'): FPP web and API are running." | tee -a "$LOGFILE"
 
 # Start the background PHP process to monitor
 /usr/bin/php ${PLUGINDIR}/rpl_run.php &
