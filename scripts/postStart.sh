84f3a91f768ecbc47292f23c54060dbafb6bfd7a
DAK Lights <github@daklights.com>
2026-06-17 21:37:03 -0400

    Debug output

--- scripts/postStart.sh
+++ scripts/postStart.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-/bin/php /home/fpp/media/plugins/randolphParkLights/rpl_run.php &
+PLUGINDIR=/home/fpp/media/plugins/randolphParkLights
+PIDFILE=${PLUGINDIR}/randolphParkLights.pid
+LOGFILE=/home/fpp/media/logs/randolphParkLights.log
+
+nohup /usr/bin/php /home/fpp/media/plugins/randolphParkLights/rpl_run.php >> "$LOGFILE" 2&>1 < /dev/null &
+echo $! > "$PIDFILE"
 
 #postStart
 
