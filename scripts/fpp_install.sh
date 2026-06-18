0464de098d3341a9f58aae3dc7013c2e9fd61cd6
DAK Lights <github@daklights.com>
2026-06-17 22:13:22 -0400

    Debug output

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Running install script for Randolph Park Lights plugin"
+
 #${FPPDIR}/scripts/ManageApacheContentPolicy.sh add connect-src https://dakjr.com 2>/dev/null || true
 
 #setSetting restartFlag 1
--- scripts/postStart.sh
+++ scripts/postStart.sh
@@ -4,11 +4,13 @@ PLUGINDIR=/home/fpp/media/plugins/randolphParkLights
 PIDFILE=${PLUGINDIR}/randolphParkLights.pid
 LOGFILE=/home/fpp/media/logs/randolphParkLights.log
 
+echo "$(date '+%Y-%m-%d %H:%M:%S'): POST START BEGIN"
 echo "$(date '+%Y-%m-%d %H:%M:%S'): POST START BEGIN" >> "$LOGFILE"
 
 nohup /usr/bin/php ${PLUGINDIR}/rpl_run.php >> "$LOGFILE" 2&>1 < /dev/null &
 echo $! > "$PIDFILE"
 
+echo "$(date '+%Y-%m-%d %H:%M:%S'): POST START COMPLETE"
 echo "$(date '+%Y-%m-%d %H:%M:%S'): POST START COMPLETE" >> "$LOGFILE"
 
 #postStart
