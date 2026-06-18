d97e0ce7e1fb55e3bd9a2b1c5b7fe29fe2834f87
DAK Lights <github@daklights.com>
2026-06-17 22:09:15 -0400

    Fix installer script

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-${FPPDIR}/scripts/ManageApacheContentPolicy.sh add connect-src https://dakjr.com 2>/dev/null || true
+#${FPPDIR}/scripts/ManageApacheContentPolicy.sh add connect-src https://dakjr.com 2>/dev/null || true
 
-setSetting restartFlag 1
+#setSetting restartFlag 1
 
 #fpp_install
