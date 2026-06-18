8152beb3520dd890f444fdb3d9b1df684a300536
DAK Lights <github@daklights.com>
2026-06-18 10:23:54 -0400

    Fix sleep syntax

--- scripts/postStart.sh
+++ scripts/postStart.sh
@@ -6,7 +6,7 @@ LOGFILE=/home/fpp/media/logs/randolphParkLights.log
 
 # Wait 15 seconds for FPP to fully boot and start
 echo "$(date '+%Y-%m-%d %H:%M:%S'): Waiting 15 seconds to ensure FPP is booted and started..." | tee -a "$LOGFILE"
-sleep(15)
+sleep 15
 
 # Start the background PHP process to monitor
 /usr/bin/php ${PLUGINDIR}/rpl_run.php &
