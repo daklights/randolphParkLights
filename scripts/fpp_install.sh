e3c43dd3c5a0efed6abeb90ae85e4a4c6f32170d
DAK Lights <github@daklights.com>
2026-06-17 22:21:13 -0400

    Changing startup

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-echo "Running install script for Randolph Park Lights plugin"
+PLUGINDIR=/home/fpp/media/plugins/randolphParkLights
+LOGFILE=/home/fpp/media/logs/randolphParkLights.log
+
+echo "$(date '+%Y-%m-%d %H:%M:%S'): FPP_INSTALL BEGIN"
+echo "$(date '+%Y-%m-%d %H:%M:%S'): FPP_INSTALL BEGIN" >> "$LOGFILE"
 
 #${FPPDIR}/scripts/ManageApacheContentPolicy.sh add connect-src https://dakjr.com 2>/dev/null || true
 
 #setSetting restartFlag 1
+
+echo "$(date '+%Y-%m-%d %H:%M:%S'): FPP_INSTALL COMPLETE"
+echo "$(date '+%Y-%m-%d %H:%M:%S'): FPP_INSTALL COMPLETE" >> "$LOGFILE"
 
 #fpp_install
--- scripts/postStart.sh
+++ scripts/postStart.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 PLUGINDIR=/home/fpp/media/plugins/randolphParkLights
-PIDFILE=${PLUGINDIR}/randolphParkLights.pid
 LOGFILE=/home/fpp/media/logs/randolphParkLights.log
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): POST START BEGIN"
 echo "$(date '+%Y-%m-%d %H:%M:%S'): POST START BEGIN" >> "$LOGFILE"
 
-nohup /usr/bin/php ${PLUGINDIR}/rpl_run.php >> "$LOGFILE" 2&>1 < /dev/null &
-echo $! > "$PIDFILE"
+/usr/bin/php ${PLUGINDIR}/rpl_run.php &
+echo $! > "$LOGFILE"
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): POST START COMPLETE"
 echo "$(date '+%Y-%m-%d %H:%M:%S'): POST START COMPLETE" >> "$LOGFILE"
